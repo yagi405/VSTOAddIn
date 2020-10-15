1618ac099603d498b2f998ccc5291d752c160828
h-saito <h-saito@powersolutions.co.jp>
2020-10-15 11:16:14 +0900

    add SampleRibbon

---
+++ VstoAddIn/HelloWorld/SampleRibbon.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Office.Tools.Ribbon;
+
+namespace HelloWorld
+{
+    public partial class SampleRibbon
+    {
+        private void SampleRibbon_Load(object sender, RibbonUIEventArgs e)
+        {
+
+        }
+
+        private void btnHello_Click(object sender, RibbonControlEventArgs e)
+        {
+            var activeSheet = Globals.ThisAddIn.Application.ActiveSheet;
+            activeSheet.Range["A1"].Cells.Value = "Hello World!";
+        }
+    }
+}
